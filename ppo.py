e8d8a25bebc25d7917b04ff352b3bf9ca4c8fe7f
Omer Sella <omer.s.sella@gmail.com>
2021-03-25 21:20:27 +0000

    working on ppo.py

--- ppo.py
+++ ppo.py
@@ -13,6 +13,7 @@ epochs = 5
 numberOfStepsPerEpoch = 10
 seed = 7134066
 localRandom = np.random.RandomState(seed)
+maximumEpisodeLength = 3
 
 def ppo():
     
@@ -22,6 +23,11 @@ def ppo():
         for t in range(numberOfStepsPerEpoch):
             vector = np.zeros(511, dtype = int)
             i, j, k, logpI, logpJ, logpK = myActorCritic.step(torch.as_tensor(observation))
+            
+            ## I don't have a critic to give value estimation yet, so set value to 1
+            value = 1
+            
+            logProbabilityList = [logpI, logpJ, logpK]
             ## Omer Sella: temporarily generate a random sparse vector
             xCoordinate = utilityFunctions.numToBits(i, 1)
             yCoordinate = utilityFunctions.numToBits(j, 4)
@@ -29,5 +35,25 @@ def ppo():
             newVector[hotBits] = 1
             nextObservation, reward, done, _ = env.step(i, j, newVector)
             
+            ## book keeping
             episodeReturn = episodeReturn + reward
             episodeLength = episodeLength + 1
+            
+            ## save to buffer and log
+            ppoBuffer.store(observation, action, reward, logProbabilityList)
+            logger.store(value)
+            
+            # Update observation to be nextObservation
+            observation = nextObservation
+            
+            timeOut = (episodeLength == maximumEpisodeLength)
+            terminate = (done or timeOut)
+            epochEnding = (t == numberOfStepsPerEpoch - 1)
+            
+            if terminate or epochEnding:
+                if timeOut or epochEnding:
+                    # Get value estimation
+                    value = 2
+                else:
+                    value = 0
+                
