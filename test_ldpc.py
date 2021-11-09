107bd5045e2645b9f15a0fa1fe9e2e5a9440da5a
Omer Sella <omer.s.sella@gmail.com>
2021-11-09 13:08:24 +0000

    changed test_ldpc

--- test_ldpc.py
+++ test_ldpc.py
@@ -5,35 +5,20 @@ Created on Mon Feb 24 17:30:45 2020
 @author: Omer
 """
 
-import ldpc
-
-
-
-
-def test_modulationAndSlicingRoundTrip():
-    assert ldpc.testModulationAndSlicingRoundTrip() == 'OK'
-import fileHandler
-#import ldpcTF
-
-  
-def test_fileHandler_nearEarth():
-    assert fileHandler.testFileHandler() == 'OK'
-    
-#def test_testModulationAndSlicingRoundtripTFimplementation():
-#    assert ldpcTF.testModulationAndSlicingRoundtrip() == 'OK'
-    
-#def test_minSumStepTFimplementation():    
-#    assert ldpcTF.testMinSumStep == 'OK'
 
 import ldpcCUDA
-
-
 def test_ldpcCUDAdecoder():
     _, status = ldpcCUDA.testNearEarth(graphics = False)
     assert status =='OK'
 import models
 
-    
+def test_roundtripCompression():
+    import ldpc_env
+    status = ldpc_env.testCompressionRoundrip()
+    assert status == 'OK'
+
+def test_fileHandler_nearEarth():
+    assert fileHandler.testFileHandler() == 'OK'
     
 def test_models():
     status = models.testActorCritic()
@@ -44,11 +29,27 @@ def test_utilityFunctionLogger():
     status = utilityFunctions.testLogger()
     assert status == 'OK'
     
+
+import ldpc
+def test_modulationAndSlicingRoundTrip():
+    assert ldpc.testModulationAndSlicingRoundTrip() == 'OK'
+import fileHandler
+#import ldpcTF
+
+  
+
+#def test_testModulationAndSlicingRoundtripTFimplementation():
+#    assert ldpcTF.testModulationAndSlicingRoundtrip() == 'OK'
+    
+#def test_minSumStepTFimplementation():    
+#    assert ldpcTF.testMinSumStep == 'OK'
+
+
+
+    
+    
+
 #def test_utilityFunctionPlotter():
 #    status = utilityFunctions.testPlotter()
 #    assert status == 'OK'
 
-def test_roundtripCompression():
-    import ldpc_env
-    status = ldpc_env.testCompressionRoundrip()
-    assert status == 'OK'
