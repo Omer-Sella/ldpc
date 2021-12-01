0f99d31d9e98494cea388a3c1fc65123d2a26555
Omer Sella <omer.s.sella@gmail.com>
2021-12-01 14:29:11 +0000

    debugging

--- envContainer.py
+++ envContainer.py
@@ -1,6 +1,7 @@
 ### Environment wrapper
 import gym
 import concurrent.futures
+import numpy as np
 
 class environmentVector():
     def __init__(self, environmentGenerationFunction, seeds, cudaDeviceList):
@@ -10,15 +11,15 @@ class environmentVector():
         for i in range(self.numberOfEnvironments):
             self.environmentList.append(environmentGenerationFunction(seeds[i], cudaDeviceList[i]))
             self.environmentIndecies.append(i)
-    return
+        return
 
     def singleReset(self, index):
         observation = self.environmentList[index].reset()
-    return observation
+        return observation
 
     def singleStep(self, action, index):
-        observedState, reward, done, flags = self.environmentList[index].step(asction)
-    return observedState, reward, done, {}
+        observedState, reward, done, flags = self.environmentList[index].step(action)
+        return observedState, reward, done, {}
 
  
 
@@ -26,7 +27,7 @@ class multiDeviceEnvironment():
     def __init__(self, environmentGenerationFunction, seeds, cudaDeviceList):
         self.environmentVector = environmentVector(environmentGenerationFunction, seeds, cudaDeviceList)
         self.indexList = self.environmentVector.environmentIndecies
-    return
+        return
 
     def reset(self):
         with concurrent.futures.ProcessPoolExecutor() as executor:    
@@ -44,9 +45,10 @@ class multiDeviceEnvironment():
 def testEnvironmentVector():
     # Init an environmentVector containing as many environments as there are cuda devices
     # Check some parameters and functionality
+    from utilityFunctions import numToBits
     from numba import cuda
     assert (len(cuda.gpus) > 0 ), "Omer Sella: either importing cuda from numba didn't work, or there are 0 cuda devices detectable."
-    environmentGenerationFunction = lambda x = 8200, y = 0: gym.make(args.env, seed = x, gpuDevice = y)
+    environmentGenerationFunction = lambda x = 8200, y = 0: gym.make('gym_ldpc:ldpc-v0', seed = x, gpuDevice = y)
     seeds = [61017406, 7134066, 90210, 42]
     # Make sure we have enough seeds for the insane case where there are more than 4 devices on a machine
     if len(cuda.gpus) > len(seeds):
@@ -54,25 +56,24 @@ def testEnvironmentVector():
     envVecor = environmentVector(environmentGenerationFunction, seeds, list(range(len(cuda.gpus))))
     for i in list(range(len(cuda.gpus))):
         print("Seed for the " + str(i) + "th environment is : ")
-        print(environmentContainer.environmentList[i].seed)
+        print(envVecor.environmentList[i].seed)
     for i in list(range(len(cuda.gpus))):
         print("Attempting reset the " + str(i) + "th environment: ")
-        print(environmentContainer.singleReset(i))
+        print(envVecor.singleReset(i))
     
+    NUMBER_OF_HOT_BITS = 7
     localRandom = np.random.RandomState(0)
     [i] = localRandom.choice(2, 1)
     [j] = localRandom.choice(16, 1)
-    [numOfHotBits] = localRandom.choice(hotBitsVector, 1)
-    hotBits = localRandom.choice(511, numOfHotBits, replace = False)
     vector = np.zeros(511, dtype = int)
     xCoordinate = numToBits(i, 1)
     yCoordinate = numToBits(j, 4)
-    hotBits = localRandom.choice(511, numOfHotBits, replace = False)
+    hotBits = localRandom.choice(511, NUMBER_OF_HOT_BITS, replace = False)
     vector[hotBits] = 1
     action = np.hstack((np.hstack((xCoordinate, yCoordinate)), vector))
     for i in list(range(len(cuda.gpus))):
         print("Attempting step for the " + str(i) + "th environment: ")
-        print(environmentContainer.singleStep(action, i))
+        print(envVecor.singleStep(action, i))
     return
     
 
