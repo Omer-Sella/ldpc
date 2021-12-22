6c61f89459f826a7d9dfdefa27b5e79389c424e0
Omer Sella <omer.s.sella@gmail.com>
2021-12-22 13:49:52 +0000

    rapid

--- ldpcCUDA.py
+++ ldpcCUDA.py
@@ -861,7 +861,7 @@ def testConcurrentFutures(numberOfCudaDevices = 1):
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = {executor.submit(evaluateCodeCuda, seeds[deviceNumber], roi, numOfIterations, nearEarthParity, numOfTransmissions, 'None', deviceNumber): deviceNumber for deviceNumber in range(numberOfCudaDevices)}
         for result in concurrent.futures.as_completed(results):
-            print(r)
+            print(result)
     ##################
 
     # OSS: disabling the "map" approach
