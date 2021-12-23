934e78e41a746e109e44ce1103185b8ef8d1f6e8
Omer Sella <omer.s.sella@gmail.com>
2021-12-23 14:57:48 +0000

    rapid

--- ldpcCUDA.py
+++ ldpcCUDA.py
@@ -856,7 +856,7 @@ def testConcurrentFutures(numberOfCudaDevices = 1):
         print(results)
         for result in concurrent.futures.as_completed(results):
             print(result)
-            a = result.berStatistics
+            a = result[0]
             print(a)
             berStats = berStats.add(result)
     ##################
