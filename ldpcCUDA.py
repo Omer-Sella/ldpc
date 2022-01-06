0d92f7f44afb09a2b2b2ed55cc0b9e59f8aca847
Omer Sella <omer.s.sella@gmail.com>
2022-01-06 16:23:03 +0000

    attempting to fix numba_cuda_multiprocessing issues using a compiler lock

--- ldpcCUDA.py
+++ ldpcCUDA.py
@@ -35,6 +35,11 @@ sys.path.insert(1, projectDir)
 
 def evaluateCodeCuda(seed, SNRpoints, numberOfIterations, parityMatrix, numOfTransmissions, G = 'None' , cudaDeviceNumber = 0):
     from numba import cuda, float32, int32
+    cuda.select_device(cudaDeviceNumber)
+    device = cuda.get_current_device()
+    print("*** debugging mp issues: "+ str(device))
+    print("*** debugging mp issues: "+ str(cuda.gpus))
+    
     LDPC_LOCAL_PRNG = np.random.RandomState(7134066)
     LDPC_MAX_SEED = 2**31 - 1
     LDPC_SEED_DATA_TYPE = np.int64
@@ -98,8 +103,7 @@ def evaluateCodeCuda(seed, SNRpoints, numberOfIterations, parityMatrix, numOfTra
     #https://github.com/numba/numba/issues/1711
     
     
-    cuda.select_device(cudaDeviceNumber)
-    device = cuda.get_current_device()
+    
     
     with compilerLock:
 
