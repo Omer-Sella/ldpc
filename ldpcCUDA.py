10db2760270f15d2fec5ae89adb4f280678c7a9f
Omer Sella <omer.s.sella@gmail.com>
2022-01-06 14:13:37 +0000

    rapid

--- ldpcCUDA.py
+++ ldpcCUDA.py
@@ -874,9 +874,9 @@ def evaluateCodeCudaWrapper(seeds, SNRpoints, numberOfIterations, parityMatrix, 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = {executor.submit(evaluateCodeCuda, seeds[deviceNumber], SNRpoints, numberOfIterations, parityMatrix, newNumOfTransmissions, 'None', deviceNumber): deviceNumber for deviceNumber in range(numberOfCudaDevices)}
         print(results)
-        for result in concurrent.futures.as_completed(results):
-            print(result.result())
-            berStats = berStats.add(result.result())
+    for result in concurrent.futures.as_completed(results):
+        print(result.result())
+        berStats = berStats.add(result.result())
    # 
    
     
