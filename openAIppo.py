a5dadff81cde038ec56c0a4811be29e7fff87a73
Omer Sella <omer.s.sella@gmail.com>
2022-01-07 09:01:24 +0000

    attempting to fix numba_cuda_multiprocessing issues using a compiler lock

--- openAIppo.py
+++ openAIppo.py
@@ -463,6 +463,13 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
                     logger.store(EpRet=ep_ret, EpLen=ep_len)
                 print("*** Debugging conc futures issues. Am I getting here ?")
                 o, ep_ret, ep_len = env.reset(), 0, 0
+
+                #############################
+                ## For debug puposes only ! 
+                ## Debugging conc futures
+                next_o, r, d, _ = env.step(a[-1])
+                ## Did it work ?
+                #############################
             
             
 
