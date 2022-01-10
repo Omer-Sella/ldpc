f0eba1b964f52af0db5873254f3e516381243832
Omer Sella <omer.s.sella@gmail.com>
2022-01-10 16:16:15 +0000

    restarting multiprocessing work

--- logx.py
+++ logx.py
@@ -163,7 +163,9 @@ class Logger:
         config_json = convert_json(config)
         if self.exp_name is not None:
             config_json['exp_name'] = self.exp_name
-        if proc_id()==0:
+        #OSS 10/01/2022 removed proc_id since no mpi
+        if True:
+        #if proc_id()==0:
             output = json.dumps(config_json, separators=(',',':\t'), indent=4, sort_keys=True)
             print(colorize('Saving config:\n', color='cyan', bold=True))
             print(output)
