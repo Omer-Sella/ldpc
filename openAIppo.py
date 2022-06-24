20305b9b2dd80ffee5c200bfb55496277c720a83
Omer-Sella <omer.s.sella@gmail.com>
2022-06-24 20:51:15 +0100

    phasing out noAction

--- openAIppo.py
+++ openAIppo.py
@@ -142,7 +142,7 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         max_ep_len=1000,
         target_kl=0.01, logger_kwargs=dict(), 
         save_freq=10, envCudaDevices = 4, experimentDataDir = None,
-        entropyCoefficient0 = 0.01, entropyCoefficient1 = 0.01, entropyCoefficient2 = 0.01, entropyCoefficientNoAction = 0.01, policyCoefficient = 1.0, resetType = 'WORST_CODES', actionInvalidator = 'Enabled'):
+        entropyCoefficient0 = 0.01, entropyCoefficient1 = 0.01, entropyCoefficient2 = 0.01, entropyCoefficientNoAction = 0.01, policyCoefficient = 1.0, resetType = 'WORST_CODES', actionInvalidator = 'Disabled'):
     """
     Proximal Policy Optimization (by clipping), 
 
@@ -322,7 +322,9 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         iEntropy = actorEntropyList[0].mean().item()
         jEntropy = actorEntropyList[1].mean().item()
         kEntropy = actorEntropyList[2].mean().item()
-        noActionEntropy = actorEntropyList[3].mean().item()
+        if actionInvalidator == 'Enabled':
+            noActionEntropy = actorEntropyList[3].mean().item()
+
         #coordinatesEntropy = actorEntropyList[3].mean().item()
        
         
@@ -339,7 +341,10 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         #########
         #OSS: we are testing a hypothesis, that the entropy for choice of i collapses too fast. So I'm replacing ent with iEntropy and let's see what happens
         #totalLoss = policyCoefficient * loss_pi + entropyCoefficient * ent
-        totalLoss = policyCoefficient * loss_pi + entropyCoefficient0 * iEntropy + entropyCoefficient1 * jEntropy + entropyCoefficient2 * kEntropy + entropyCoefficientNoAction * noActionEntropy
+        if actionInvalidator == 'Enabled':
+            totalLoss = policyCoefficient * loss_pi + entropyCoefficient0 * iEntropy + entropyCoefficient1 * jEntropy + entropyCoefficient2 * kEntropy + entropyCoefficientNoAction * noActionEntropy
+        else:
+            totalLoss = policyCoefficient * loss_pi + entropyCoefficient0 * iEntropy + entropyCoefficient1 * jEntropy + entropyCoefficient2 * kEntropy
         
         return totalLoss, pi_info
 
