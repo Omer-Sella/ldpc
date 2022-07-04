76f7e9326f09c6ffc849db2777eb1033ba957a60
Omer-Sella <oss22@cam.ac.uk>
2022-07-04 09:22:12 +0100

    characterised ldpc.py

--- ldpc.py
+++ ldpc.py
@@ -399,9 +399,10 @@ def evaluateCodeWrapper(seed, SNRpoints, numberOfIterations, parityMatrix, numOf
     berStats = common.berStatistics()
     #It is assumed, i,.e., no safety, that the number of cores is => number of transmissions !!!!
     newNumOfTransmissions = numOfTransmissions // numberOfCores
-    
-    messageSize = 16*1021- (2*1021)
-    codewordSize = 16*1021
+    circulantSize = 511
+    #circulantSize = 1021
+    messageSize = 16 * circulantSize - (2 * circulantSize)
+    codewordSize = 16 * circulantSize
     
     #Temporarily disabled for debug of cu_init error
     #print("*** debugging multiple futures. NumberOfCudaDevices: " + str(numberOfCudaDevices))
@@ -591,7 +592,7 @@ def main():
 
     for i in [1,2,4,7,8, 14, 28, 56]:
         start = time.time()
-        bStats = evaluateCodeWrapper(seed = seed, SNRpoints = roi, numberOfIterations = numOfIterations parityMatrix = nearEarthParity numOfTransmissions = numOfTransmissions, G = 'None' , numberOfCores = i)
+        bStats = evaluateCodeWrapper(seed = seed, SNRpoints = roi, numberOfIterations = numOfIterations, parityMatrix = nearEarthParity, numOfTransmissions = numOfTransmissions, G = 'None' , numberOfCores = i)
         end = time.time()
         print('Time it took for code evaluation == %d' % (end-start))
         print('Throughput == '+str((8176*len(roi)*numOfTransmissions)/(end-start)) + 'bits per second.')
